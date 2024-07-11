1293b675d0de66ef95f6c47493bd35fd80cb1cc8
summereuna <summereuna@gmail.com>
2024-07-11 16:01:35 +0900

    [Feat] : 리액트 라우터 Outlet 연습 나중에 삭제할 수도

--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -1,15 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "@pages/Home";
 import TodoDetail from "@pages/TodoDetail";
-import Layout from "@components/Layout";
+import Layout from "@shared/Layout";
+
+import DashBoardLayout from "@shared/DashBoardLayout";
+import Mypage from "@pages/Mypage";
+import Like from "@pages/Like";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Layout>
         <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/:id" element={<TodoDetail />}></Route>
+          <Route path="/" element={<Home />} />
+
+          <Route path="/dashboard" element={<DashBoardLayout />}>
+            <Route path="/dashboard/mypage" element={<Mypage />}></Route>
+            <Route path="/dashboard/like" element={<Like />}></Route>
+          </Route>
+
+          <Route path="/:id" element={<TodoDetail />} />
         </Routes>
       </Layout>
     </BrowserRouter>
